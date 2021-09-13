9677a56183caf37deefba2bd0274732a42df978c
Quickz <alexserg@inbox.lv>
2021-09-13 17:02:15 +0300

    added a blank project

---
+++ ToDoListConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ToDoListConsoleApp
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+            Console.ReadKey();
+        }
+    }
+}
